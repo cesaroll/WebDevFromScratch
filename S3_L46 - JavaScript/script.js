0dca86999d366ed00fc349fbd992623d21e8b2ba
Cesar Lopez <cesarl@outlook.com>
2015-07-13 17:41:12 -0600

    S3-L46 : Intro to JavaScript

---
+++ S3_L46 - JavaScript/script.js
@@ -0,0 +1,20 @@
+/* Multiline comment
+    Example
+*/
+document.write("Hello World - from External .js file <br /><br />"); //Single line comment
+
+/* Variables */
+var x = 3;
+var y = 1;    
+document.write(x+y);
+
+var candy = 3.21;
+var shoe = 15.22;
+
+document.write("<br />");
+document.write(candy+shoe);
+
+
+var title = "News on Monday";
+var desc = "Someone got killed";
+document.write("<br />"+"<h1>"+title+"</h1>"+desc);

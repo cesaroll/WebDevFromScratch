c688f93842b75e5f588879058619d49ef0a3e6a9
Cesar Lopez <cesarl@outlook.com>
2015-07-22 21:59:20 -0600

    Added Alerts and Functions.

--- S3_L46 - JavaScript/script.js
+++ S3_L46 - JavaScript/script.js
@@ -6,7 +6,7 @@ document.write("Hello World - from External .js file <br /><br />"); //Single li
 /* Variables */
 var x = 3;
 var y = 1;    
-document.write(x+y);
+document.write(x + y);
 
 var candy = 3.21;
 var shoe = 15.22;
@@ -17,5 +17,6 @@ document.write(candy+shoe);
 
 var title = "News on Monday";
 var desc = "Someone got killed";
+
 document.write("<br />"+"<h1>"+title+"</h1>"+desc);
 

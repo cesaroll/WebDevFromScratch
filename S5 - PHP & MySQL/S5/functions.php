3051e846a0322445c9eb619db38a3568fe6cca22
Cesar Lopez <cesarl@outlook.com>
2015-08-04 16:13:26 -0600

    S5 : Functions

---
+++ S5 - PHP & MySQL/S5/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function my_car($arg1, $arg2) {
+    
+    echo "My ".$arg1." is really ".$arg2."!<br/>";
+}
+
+my_car("car", "cool");
+my_car("wife", "beautifull");
+my_car("house", "nice");
+
+?>

8781b7fbfdda18ca3aca1961d4ffbc535a8b1836
Cesar Lopez <cesarl@outlook.com>
2015-08-04 16:05:29 -0600

    S5 : Storing Html in variables

---
+++ S5 - PHP & MySQL/S5DW2/allsections.php
@@ -0,0 +1,30 @@
+
+
+<?php
+
+$head_section = '<head>
+	<title>Sample</title>	
+	<link rel="stylesheet" href="styles/style.css" type="text/css" />
+</head>';
+
+$body = '
+	<div id="header">
+		<h1>My Website</h1>
+	</div>
+	<div id="navigation">
+		<ul>
+			<li><a href="section1.php">Section 1</a></li>
+			<li><a href="section2.php">Section 2</a></li>
+			<li><a href="section3.php">Section 3</a></li>
+			<li><a href="section4.php">Section 4</a></li>
+			<li><a href="#">Coming Soon</a></li>
+		</ul>
+	</div>';
+
+$content = '<div id="content">Welcome! Choose a section on the manu ...</div>';
+
+$footer = '<div id="footer">
+		<p>Copyright &copy; <a href="#">My company</a> 2014 | Designed by Myself | Sponsored by my friends | Valid <a href="http://jigsaw.w3.org/css-validator/">CSS</a> &amp; <a href="http://validator.w3.org/">XHTML</a></p>
+	</div>';
+
+?>
---
+++ S5 - PHP & MySQL/S5DW2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<?php include('allsections.php');?>
+
+<?php echo $head_section;?>
+
+<body>
+
+<?php
+
+echo $body;
+
+echo $content;
+
+echo $footer;
+
+?>
+
+</body>
+
+</html>

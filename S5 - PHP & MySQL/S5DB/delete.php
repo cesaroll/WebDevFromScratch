32244b748a0bac2d7681ffe5fdea4021170532e5
Cesar Lopez <cesarl@outlook.com>
2015-08-06 13:54:20 -0600

    S5 : Delete users passing values through URL

--- S5 - PHP & MySQL/S5DB/delete.php
+++ S5 - PHP & MySQL/S5DB/delete.php
@@ -4,9 +4,9 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
     
     include("connection.php");
     
-    $getemail = mysqli_real_escape_string($dbc,trim($_POST['getemail']));
+    $user_id = mysqli_real_escape_string($dbc,trim($_POST['user_id']));
     
-    mysqli_query($dbc, "DELETE FROM users WHERE email='$getemail' ");
+    mysqli_query($dbc, "DELETE FROM users WHERE id='$user_id' ");
     
     echo "User has been deleted!";
     
@@ -19,9 +19,11 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
 ?>
 
 
-<h3>Type email of user to be deleted:</h3>
+<h3>Are you sure you want to delete this user?</h3>
 <form method="post" action="delete.php">
-    
-    <input type="text" name="getemail">
-    <input type="submit" name="submit" value="Delete this User">
+    <p>User ID:<input type="text" name="user_id" value="<?php echo $_GET['user_id']; ?>" /></p>
+    <p>First Name: <input type="text" name="first_name" value="<?php echo $_GET['fname']; ?>" /></p>
+    <p>Last Name: <input type="text" name="last_name" value="<?php echo $_GET['lname']; ?>"/></p>
+    <p><input type="submit" name="submit" value="Yes"></p>
+    <p><a href="output.php" >Go Back</a></p>
 </form>
--- S5 - PHP & MySQL/S5DB/output.php
+++ S5 - PHP & MySQL/S5DB/output.php
@@ -1,29 +1,43 @@
-
-<table align="center" border="1" cellspacing="3" cellpading="3" width="75%">
-    <tr>
-    <th>Name</th>
-
-    <th>Email</th>
-    </tr>
-
 <?php
 
 include("connection.php");
 
-$reader = mysqli_query($dbc, "SELECT * FROM users ORDER BY last_name");
+$query = "SELECT id, first_name, last_name, email, DATE_FORMAT(registration_date, '%M %d, %Y') AS regis_dte ";
+$query .= " FROM users ORDER BY registration_date ";
 
-while($row = mysqli_fetch_array($reader) ) {
+$reader = mysqli_query($dbc, $query);
+$count = mysqli_num_rows($reader);
+
+// If rows returned, display records
+if($count > 0) {
     
-    echo "<tr>";
-    echo "<td>".$row["last_name"].", ".$row["first_name"];
-    echo "</td>";
-    echo "<td>".$row["email"]."</td>";
-    echo "</tr>";
+    echo "<table align='center' border='1' cellspacing='3' cellpading='3' width='75%'>
+    <tr><th>Edit</th><th>Delete</th><th>Name</th><th>Email</th><th>Date Registered</th></tr>";
+    
+    while($row = mysqli_fetch_array($reader) ) {
+    
+        echo "<tr>";
+        echo "<td><a href='edit_user.php?user_id=".$row["id"]."&fname=".$row["first_name"]."&lname=".$row["last_name"]."' />Edit</td>";
+        echo "<td><a href='delete.php?user_id=".$row["id"]."&fname=".$row["first_name"]."&lname=".$row["last_name"]."' />Delete</td>";
+        echo "<td>".$row["last_name"].", ".$row["first_name"];
+        echo "</td>";
+        echo "<td>".$row["email"]."</td>";
+        echo "<td>".$row["regis_dte"]."</td>";
+        echo "</tr>";
+    
+    }
+    
+    echo "</table>";
+    
+} else {
+ 
+    echo "<br/>There are no registered users.<br/>";
     
 }
+
+
 
 mysqli_close($dbc);
 
 ?>
 
-</table>
